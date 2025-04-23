c5a9757b2adcfe33d69f25746dd2d59b1bef731b
c22emigr <c22emigr@student.his.se>
2025-04-23 12:12:37 +0200

    added code for watchlist/favorite + ui for it
    code for watchlist/favorite in MiniDashboard.js

--- src/components/MiniDashboard.js
+++ src/components/MiniDashboard.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MiniDashboard = ({stockdata}) => {
-    if (!stockdata || stockdata.length === 0) return null;
+    const [isFavorite, setIsFavorite] = useState(false);
 
-    const latest = stockdata[stockdata.length -1];
-    const prev = stockdata[stockdata.length -2] || latest;
-    
-    const change = latest.Close - prev.Close;
+    const hasData = stockdata && stockdata.length > 0;
+
+    const latest = hasData ? stockdata[stockdata.length - 1] : {};
+    const prev = hasData ? stockdata[stockdata.length - 2] || latest : {};
+    const symbol = latest?.symbol || "";
+
+    // Dummy variables in case of no data
+    const change = latest.Close && prev.Close ? latest.Close - prev.Close : 0;
     const changePct = ((change / prev.Close) * 100).toFixed(2);
     const isPositive = change >= 0;
+
+    
+    // localStorage on load
+    useEffect(() => {
+        const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+        setIsFavorite(watchlist.includes(symbol));
+    }, [symbol]);
+
+    const toggleFavorite = () => {
+        let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+
+    if (watchlist.includes(symbol)) {
+        watchlist = watchlist.filter(sym => sym !== symbol);
+    } else {
+        watchlist.push(symbol);
+    }
+        setIsFavorite(!isFavorite);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    };
+
+    if (!hasData) return null;
 
     return (
         <div className="p-4 bg-white dark:bg-gray-800 dark:text-gray-100 rounded shadow-md w-full max-w-md mx-auto mb-7">
         <div>
             <p className="text-xl font-bold">{stockdata[0].stockname}</p>
+            <button
+                onClick={toggleFavorite}
+                className={`text-sm px-3 py-1 rounded border transition-colors duration-250 hover:border-emerald-400 hover:scale-[1.02] active:scale-[0.98] ${
+                    isFavorite
+                    ? "bg-emerald-400 text-white"
+                    : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white"
+                }`}
+                >
+                {isFavorite ? "★ Favorited" : "☆ Add to Watchlist"}
+            </button>
         </div>
         <div>
              <p className="font-medium">Price</p>
