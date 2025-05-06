bdaa2ea31dbd720c8509e2ec6afa03b28343d2bd
c22emigr <emil.gronvall18@gmail.com>
2025-05-06 22:19:06 +0200

    Improved styling of search bar

--- src/components/StockSearch.js
+++ src/components/StockSearch.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import usStocks from "../data/us_stocks.json";
 import Flag from 'react-world-flags';
+import { Search } from "lucide-react";
 
 export default function StockSearch({
     searchInput,
@@ -144,97 +145,96 @@ export default function StockSearch({
     }
 
     return (
-      <div ref={searchRef}>
+      <div ref={searchRef}> {/* CLICKING OUTSIDE CLOSES DOWN SUGGESTIONS */}
       {/* Search input*/}
-      <form className="relative flex justify-center p-2" id="searchform">
-        <div>
-          <div className="form">
-            <input
-              type="text"
-              value={searchInput}
-              id="stocksearch"
-              onKeyDown={handleKeyDown}
-              className="bg-white dark:bg-[#1d2228] p-2 rounded border"
-              onFocus={() => {
-                if (searchInput.length === 0) {
-                  setFilteredResults(popularStocksByMarket[selectedMarket] || []);
-                }
-              }}
-              onChange={(e) => {
-                const userInput = e.target.value;
-                setSearchInput(userInput);
-                setFilteredResults(filterStocks(userInput));
-                setHighlightedIndex(-1);
-                if (userInput.length > 0) {
-                  const matches = usStocks
-                  .filter((stock) => {
-                    if (selectedMarket === "" && stock.market !== "US") return false;
-                    if (selectedMarket === ".ST" && stock.market !== "SE") return false;
-                    if (selectedMarket === ".T" && stock.market !== "JP") return false;
-                    if (selectedMarket === ".TO" && stock.market !== "CA") return false;
-                    if (selectedMarket === ".F" && stock.market !== "DE") return false;
-                    if (selectedMarket === ".L" && stock.market !== "GB") return false;
-                    if (selectedMarket === ".PA" && stock.market !== "FR") return false;
-                    if (selectedMarket === ".AX" && stock.market !== "AU") return false;
-                    if (selectedMarket === ".HK" && stock.market !== "HK") return false;
-                    return true;
-                  })
-                  .filter((stock) =>
-                    stock.name.toLowerCase().includes(userInput.toLowerCase())
-                  );
-                  setFilteredResults(matches);
-                  setHighlightedIndex(-1); // Resets the highlight
-                } else {
-                  setFilteredResults([]);
-                }
-              }}
-              placeholder="Enter company name"
-            />
+      <div className="relative w-full">
+        <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 dark:text-gray-500 w-4 h-4 pointer-events-none" />
+        <input
+          type="text"
+          value={searchInput}
+          onKeyDown={handleKeyDown}
+          className="w-full bg-white dark:bg-[#1d2228] p-2 pl-10 rounded border text-sm text-black dark:text-white"
+          onFocus={() => {
+            if (searchInput.length === 0) {
+              setFilteredResults(popularStocksByMarket[selectedMarket] || []);
+            }
+          }}
+          onChange={(e) => {
+            const userInput = e.target.value;
+            setSearchInput(userInput);
+            setFilteredResults(filterStocks(userInput));
+            setHighlightedIndex(-1);
+            if (userInput.length > 0) {
+              const matches = usStocks
+                .filter((stock) => {
+                  if (selectedMarket === "" && stock.market !== "US") return false;
+                  if (selectedMarket === ".ST" && stock.market !== "SE") return false;
+                  if (selectedMarket === ".T" && stock.market !== "JP") return false;
+                  if (selectedMarket === ".TO" && stock.market !== "CA") return false;
+                  if (selectedMarket === ".F" && stock.market !== "DE") return false;
+                  if (selectedMarket === ".L" && stock.market !== "GB") return false;
+                  if (selectedMarket === ".PA" && stock.market !== "FR") return false;
+                  if (selectedMarket === ".AX" && stock.market !== "AU") return false;
+                  if (selectedMarket === ".HK" && stock.market !== "HK") return false;
+                  return true;
+                })
+                .filter((stock) =>
+                  stock.name.toLowerCase().includes(userInput.toLowerCase())
+                );
+              setFilteredResults(matches);
+              setHighlightedIndex(-1); // Resets the highlight
+            } else {
+              setFilteredResults([]);
+            }
+          }}
+          placeholder="Enter company name"
+        />
+      </div>
 
 
-            {/* Suggested Results */}
-            {filteredResults.length > 0 && (
-              <ul className="absolute z-50 overflow-y-auto w-full max-h-96 scroll-smooth pointer-events-auto rounded shadow-md backdrop-blur-md">
-                {filteredResults.map((stock, index) => (
-                  <li
-                    key={index}
-                    className={`p-2 dark:text-white text-black cursor-pointer text-sm ${
-                      index === highlightedIndex
-                      ? "bg-emerald-400 text-gray-700"
-                      : "hover:bg-gray-100 dark:hover:bg-gray-700"
-                    }`}
-                    onMouseEnter={() => setHighlightedIndex(index)}
-                    onClick={() => {
-                      setSearchInput(stock.name);
-                      setSelectedSymbol(stock.symbol);
-                      setFilteredResults([]);
+      {/* Suggested Results */}
+      {filteredResults.length > 0 && (
+        <ul className="absolute z-50 w-full max-w-[300px] overflow-y-auto max-h-96 scroll-smooth pointer-events-auto rounded shadow-md backdrop-blur-md">
+          {filteredResults.map((stock, index) => (
+            <li
+              key={index}
+              className={`p-2 dark:text-white text-black cursor-pointer text-sm ${
+                index === highlightedIndex
+                ? "bg-emerald-400 text-gray-700"
+                : "hover:bg-gray-100 dark:hover:bg-gray-700"
+              }`}
+              onMouseEnter={() => setHighlightedIndex(index)}
+              onClick={() => {
+                setSearchInput(stock.name);
+                setSelectedSymbol(stock.symbol);
+                setFilteredResults([]);
+              }}
+            >
+              <div className="flex items-center gap-2">
+                {/* Flag here */}
+                <Flag
+                  code={getCountryCodeFromMarket(stock.market)}
+                  style={{ width: 20 }}
+                />
+                {stock.name} <span className="text-gray-900 dark:text-gray-100">({stock.symbol})</span>
+                {comparedSymbols.length < 2 && (
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation(); // So it doesn't trigger the li click
+                      handleCompareAndNavigate(stock.symbol, selectedMarket);
                     }}
+                    className="text-green-500 hover:text-green-700 ml-2"
                   >
-                    <div className="flex items-center gap-2">
-                      {/* Flag here */}
-                      <Flag
-                        code={getCountryCodeFromMarket(stock.market)}
-                        style={{ width: 20 }}
-                      />
-                      {stock.name} <span className="text-gray-900 dark:text-gray-100">({stock.symbol})</span>
-                      {comparedSymbols.length < 2 && (
-                        <button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation(); // So it doesn't trigger the li click
-                            handleCompareAndNavigate(stock.symbol, selectedMarket);
-                          }}
-                          className="text-green-500 hover:text-green-700 ml-2"
-                        >
-                          +
-                        </button>
-                      )}
-                      
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
+                    +
+                  </button>
+                )}
+                
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
             
 
             <button
@@ -245,9 +245,5 @@ export default function StockSearch({
               Search
             </button>
           </div>
-        </div>
-
-      </form>
-    </div>
     );
 }
